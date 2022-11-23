f78470471d7c5837c15fbed700fafbf8959d125d
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-11-23 03:27:47 +0200

    Time: 174 ms (25.35%), Space: 50.3 MB (90.70%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -5,21 +5,34 @@
  * @return {number}
  */
 var nearestValidPoint = function(x, y, points) {
-    let validPoints=[];
-    for(let i=0;i<points.length;i++){
-        if(points[i][0]==x||points[i][1]==y){  
-         let distance=Math.abs(x-points[i][0]+y-points[i][1]);
-        validPoints.push([distance,i])
-        }
-    }
-    if(validPoints.length==0)return -1;
-    let smallest=validPoints[0];
-    validPoints.forEach(ele=>{
-        if(ele[0]<smallest[0]){
-            smallest=ele;
+    // let validPoints=[];
+    // for(let i=0;i<points.length;i++){
+    //     if(points[i][0]==x||points[i][1]==y){  
+    //      let distance=Math.abs(x-points[i][0]+y-points[i][1]);
+    //     validPoints.push([distance,i])
+    //     }
+    // }
+    let minDistance=Infinity;
+    let idx=-1;
+    points.forEach(([a,b],i)=>{
+        if(a==x||b==y){
+            let distance=Math.abs(x-points[i][0]+y-points[i][1]);
+            if(distance<minDistance){
+                minDistance=distance;
+                idx=i;
+            }   
         }
     })
-return smallest[1]
+//     if(validPoints.length==0)return -1;
+//     let smallest=validPoints[0];
+//     validPoints.forEach(ele=>{
+//         if(ele[0]<smallest[0]){
+//             smallest=ele;
+//         }
+//     })
+// return smallest[1]
+    
+    return idx;
    
 
 };
