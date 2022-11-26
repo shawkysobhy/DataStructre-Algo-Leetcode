436195dc0e57e5020bd213928a18630bc02cedd7
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-11-26 18:33:23 +0200

    Time: 114 ms (14.86%), Space: 42 MB (94.24%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let product=1;
+for(let i=0;i<nums.length;i++){
+    if(nums[i]===0){
+        return 0
+    }
+    if(nums[i]<0){
+        product=product*-1;
+    }
+}
+    if(product<0)return -1;
+    if(product>0)return 1;
+};
