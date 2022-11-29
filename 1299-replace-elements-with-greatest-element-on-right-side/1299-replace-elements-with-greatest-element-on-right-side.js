4f20355a4965e2dbace86634264a9f951196690b
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-11-29 09:16:35 +0200

    Time: 190 ms (45.52%), Space: 51.7 MB (8.69%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -2,18 +2,28 @@
  * @param {number[]} arr
  * @return {number[]}
  */
+
 var replaceElements = function(arr) {
-    for(let i=0;i<arr.length;i++){
-        if(i===arr.length-1){
-            arr[i]=-1;
-        }else{
-            arr[i]=arr[i+1];
-        }   
-        for(let j=i+1;j<arr.length;j++){
-            if(arr[j]>arr[i]){
-                arr[i]=arr[j]
-            }
-        }
+    // for(let i=0;i<arr.length;i++){
+    //     if(i===arr.length-1){
+    //         arr[i]=-1;
+    //     }else{
+    //         arr[i]=arr[i+1];
+    //     }   
+    //     for(let j=i+1;j<arr.length;j++){
+    //         if(arr[j]>arr[i]){
+    //             arr[i]=arr[j]
+    //         }
+    //     }
+    // }
+    
+    let max=-1;
+     // arr[arr.length-1]=-1;
+    for(let i=arr.length-1;i>=0;i--){
+        
+        let newMax=Math.max(arr[i],max)
+        arr[i]=max;
+        max=newMax;
     }
     return arr;
 };
