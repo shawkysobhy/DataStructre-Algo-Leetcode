6dbba7d0d8f8ebe1f8b027a535ea15378fc8983d
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-11-29 09:38:15 +0200

    Time: 127 ms (5.22%), Space: 44 MB (5.00%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let words=s.split(" ");
+    console.log(words)
+    console.log("---------")
+    let expected=words[words.length-1]
+    for(let i=words.length-1;i>=0;i--){
+        if(words[i]!==''){
+            console.log(words[i])
+            expected=words[i];
+            break;
+        }
+    }
+    return expected.length;
+};
