c9276147e0a9d5bb664a57b0186cedfa1a2283c2
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-11-29 08:33:03 +0200

    Time: 1382 ms (14.79%), Space: 51.5 MB (10.95%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function(arr) {
+    for(let i=0;i<arr.length;i++){
+        if(i===arr.length-1){
+            arr[i]=-1;
+        }else{
+            arr[i]=arr[i+1];
+        }   
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]>arr[i]){
+                arr[i]=arr[j]
+            }
+        }
+    }
+    return arr;
+};
