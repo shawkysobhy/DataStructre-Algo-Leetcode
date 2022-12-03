88fd26d138fa9af3cdad6afe03e59701bb07de69
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-12-03 22:35:43 +0200

    Time: 65 ms (89.97%), Space: 42 MB (45.95%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -6,17 +6,22 @@ var generate = function(numRows) {
     let result=[[1]];
     for(let i=1;i<numRows;i++){
          let row=[];
-        let rowCol=i+1;// define how cell in row
+        let rowCol=i+1;
         for(let j=0;j<rowCol;j++){
-            if(j==0||j+1==rowCol){
-                row.push(1); 
-            }else{
+            if(j==0){
+                row.push(1);
+
+            }else if(j+1==rowCol){
+               row.push(1)
+
+           }else{
              let val=result[i-1][j-1]+result[i-1][j];
             row.push(val) 
-           }  
+           }
+
         }
         result.push(row)
     }
-    
+
     return result;
 };
