d5db1e4e7a22bc8c35d55a4126999c304d997422
shawky  sobhy <94941783+shawky55@users.noreply.github.com>
2022-12-05 00:41:56 +0200

    Time: 168 ms (15.68%), Space: 49.6 MB (11.76%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.ts
+++ 0205-isomorphic-strings/0205-isomorphic-strings.ts
@@ -2,17 +2,13 @@ function isIsomorphic(s: string, t: string): boolean {
     let map=new Map();
     
     for(let i=0;i<s.length;i++){
-
-       let si=s[i] 
-        if(map.has(si)&&map.get(si)!==t[i]){
-            console.log('fired1')
-            return false;
-            
-        }else if([...map.values()].includes(t[i])&&![...map.keys()].includes(si)){
-            console.log(map.values())
+        if(map.has(s[i] )&&map.get(s[i] )!==t[i]){ //check smae char map to multiple chars
+            return false;     
+        }else if([...map.values()].includes(t[i])&&![...map.keys()].includes(s[i] )){
+            //check there multiple 
             return false;
         }else{
-            map.set(si,t[i])
+            map.set(s[i] ,t[i])
         }
             
     }
