c031c99598caf4593032f69418d994f03166434e
shawky  sobhy <shawkysobhy98@gmail.com>
2022-12-23 23:20:29 +0200

    https://leetcode.com/problems/valid-palindrome/submissions/864443165/

---
+++ 125-Valid Palindrome/isPalindrome.ts
@@ -0,0 +1,22 @@
+//leetcode.com/problems/valid-palindrome/submissions/864443165/
+
+https: function isPalindrome(s: string): boolean {
+    let left = 0;
+    let right = s.length - 1;
+    let w1: string;
+    let w2: string;
+    while (right > left) {
+        w1 = s[left].toLowerCase();
+        w2 = s[right].toLowerCase();
+        if (!/[a-zA-Z0-9]/.test(w1)) {
+            left++;
+        } else if (!/[a-zA-Z0-9]/.test(w2)) {
+            right--;
+        } else {
+            if (w1 !== w2) return false;
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
