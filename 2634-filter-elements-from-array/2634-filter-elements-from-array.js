563c8c4ca37e9574bfc94cb49b637e551f651e60
Shawky <94941783+shawkysobhy@users.noreply.github.com>
2023-04-27 23:13:34 +0200

    Time: 58 ms (58.83%), Space: 42.4 MB (13.52%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -7,7 +7,7 @@ var filter = function(arr, fn) {
     let filterdArr=[]
     for(let i=0;i<arr.length;i++){
         if(fn(arr[i],i)){
-            filterdArr.push(arr[i])
+            filterdArr[filterdArr.length]=arr[i]
         }
     }
     return filterdArr;
