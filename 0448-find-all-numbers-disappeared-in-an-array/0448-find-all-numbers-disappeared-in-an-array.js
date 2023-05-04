8a5e1f9a238574a3e4e316b40120504229ef2181
Shawky <94941783+shawkysobhy@users.noreply.github.com>
2023-05-04 14:44:56 +0300

    Time: 104 ms (72.22%), Space: 53.4 MB (27.28%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -3,11 +3,12 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    let output=[]
+    let output=[];
+    let setNum=new Set(nums);
     for(let i=1;i<=nums.length;i++){
-        if(!nums.includes(i)){
-            output.push(i)
-        }
+       if(!setNum.has(i)){
+           output.push(i)
+       } 
     }
     return output;
 };
