d18738896a35e1894d002897f08eeec72458c356
Shawky <94941783+shawkysobhy@users.noreply.github.com>
2023-05-04 12:46:49 +0300

    Time: 89 ms (34.89%), Space: 43.6 MB (67.55%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,6 +2,12 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
+var mySqrt = function(num) {
+    if(num==0)return 0;
+    if(num==1)return 1;
+   for(let i=0;i<num+1;i++){
+       if(i*i>num)return i-1;
+
+   }
+
 };
