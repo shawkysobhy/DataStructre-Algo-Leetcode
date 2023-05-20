2883fdbd6d582122c96300901ffb959a56c80dd3
Shawky <94941783+shawkysobhy@users.noreply.github.com>
2023-05-20 16:07:52 +0300

    Time: 114 ms (81.49%), Space: 47.6 MB (52.11%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -4,9 +4,9 @@
  */
 var arrayPairSum = function(nums) {
     nums.sort((a,b)=>a-b);
-    const minNums=[];
+    let result=0;
     for(let i=0;i<nums.length;i+=2){
-        minNums.push(Math.min(nums[i],nums[i+1]));
+      result+=Math.min(nums[i],nums[i+1]);
     }
-    return minNums.reduce((acc,curr)=>curr+acc)
+    return result;
 };
