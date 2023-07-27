64d77b0664749b1ab0fe559c757a83c1602507c8
Shawky <94941783+shawkysobhy@users.noreply.github.com>
2023-07-27 15:06:53 +0300

    Time: 121 ms (23.83%), Space: 53.1 MB (83.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,22 +1,9 @@
 function containsDuplicate(nums: number[]): boolean {
-  let numsMap=new Map();
-  for(let i=0;i<nums.length;i++){
-      let re=numsMap.get(nums[i])
-      if(!re){
-          numsMap.set(nums[i],1)
-      }else{
-          numsMap.set(nums[i],++re)
-      }
-  }
-for(let i=0;i<nums.length;i++){
-    if(numsMap.get(nums[i])>1){
-        return true;
-    }
-}
+   const sortedArr=nums.sort((a,b)=>a-b);
+   for(let i=0;i<sortedArr.length-1;i++){
+       if(sortedArr[i]===sortedArr[i+1]){
+           return true;
+       }
+   } 
   return false;
 };
-/**
-Another good solution is to sort the array
-and compare between arr[i] and arr[i+1] if they the same return true
-simple solution and shor
- */
