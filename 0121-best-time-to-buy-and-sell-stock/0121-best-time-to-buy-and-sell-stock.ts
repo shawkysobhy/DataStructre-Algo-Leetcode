4a244845c609ebc219a8c67d541424c36c14c60d
Shawky <94941783+shawkysobhy@users.noreply.github.com>
2023-08-02 14:49:49 +0300

    Time: 1666 ms (5.09%), Space: 84.3 MB (5.02%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,21 @@
+function maxProfit(prices: number[]): number {
+   let b_profit=0;
+   let b_bought=prices[0]
+   let b_sell=prices[1];
+    let pt1=0;
+    let pt2=1;
+   while(pt2<prices.length){
+       let profit=prices[pt2]-prices[pt1];  
+       console.log(`${prices[pt2]} - ${prices[pt1]} = ${profit}`)
+       if(profit>b_profit){
+           b_profit=profit;
+       }
+       if(prices[pt2]<b_bought){
+           pt1=pt2;
+           b_bought=prices[pt1]
+       }
+       pt2++;
+       
+   }
+    return b_profit;
+};
