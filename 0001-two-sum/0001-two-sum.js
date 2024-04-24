085e8c0e8a9d9496d2d9891d8373bed89b4ca5d6
Shawky Sobhy <94941783+shawkysobhy@users.noreply.github.com>
2024-04-24 21:23:10 +0200

    Time: 68 ms (51.90%), Space: 52.3 MB (7.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,16 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(arr, target) {
     let numsMap=new Map();
-    for(let i=0;i<arr.length;i++){
-        numsMap.set(arr[i],i)  
+ arr.forEach((num,i)=>{
+      numsMap.set(num,i)
+  })
+
+ for(let i=0;i<arr.length;i++){
+    let current=arr[i]
+    let secNum=target-current;
+    if(numsMap.has(secNum)&&numsMap.get(secNum)!==i){
+        return [i,numsMap.get(secNum)];      
         }
-    for(let i=0;i<arr.length;i++){
-        let secNum=target-arr[i];
-        let secIndex=numsMap.get(secNum);
-        if(secIndex!==undefined&&secIndex!==i){
-            return [i,numsMap.get(secNum)];
-        }
-    }
-};
+}}
