242301bbbcb21c13762e7266bc3b0d09e0cc4aeb
Shawky Sobhy <94941783+shawkysobhy@users.noreply.github.com>
2024-04-25 18:12:27 +0200

    [283] [Time Beats: 70.73%] [Memory Beats: 92.26%] - LeetPush

---
+++ 283-Move-Zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let ptr1=0;
+    let ptr2=1;
+    while(ptr2<nums.length){
+        if(nums[ptr1]==0&&nums[ptr2]!==0){
+        let placholder=nums[ptr2]
+        nums[ptr2]=nums[ptr1];
+        nums[ptr1]=placholder;
+        ptr1++;
+        ptr2++;
+        }else if(nums[ptr1]==0&&nums[ptr2]===0){
+            ptr2++;
+        }else {
+        ptr1++;
+        ptr2++;
+        }
+ 
+    }
+    return nums
+};
