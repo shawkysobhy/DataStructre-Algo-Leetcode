a347a10e12c94d1c077ad975f5b7bd210a348f3b
Shawky Sobhy <94941783+shawkysobhy@users.noreply.github.com>
2024-05-02 01:29:00 +0300

    [28] [Time Beats: 46.43%] [Memory Beats: 93.59%] - LeetPush

--- 28-Find-the-Index-of-the-First-Occurrence-in-a-String.js
+++ 28-Find-the-Index-of-the-First-Occurrence-in-a-String.js
@@ -3,15 +3,25 @@
  * @param {string} needle
  * @return {number}
  */
+// var strStr = function(haystack, needle) {
+//     let map=new Map();
+//     let needleLen=needle.length
+//     for(let i=0;i<haystack.length;i++){
+//         let strEnd=needleLen+i
+//         if(!map.has(haystack.substring(i,strEnd))){
+//             map.set(haystack.substring(i,strEnd),i)
+//         }
+//     }
+//     if(map.has(needle))return map.get(needle)
+//     return -1;
+// };
+
+//second soluation
 var strStr = function(haystack, needle) {
-    let map=new Map();
-    let needleLen=needle.length
-    for(let i=0;i<haystack.length;i++){
-        let strEnd=needleLen+i
-        if(!map.has(haystack.substring(i,strEnd))){
-            map.set(haystack.substring(i,strEnd),i)
-        }
-    }
-    if(map.has(needle))return map.get(needle)
-    return -1;
+      for(let i=0;i<haystack.length-needle.length+1;i++){
+       if(haystack.substring(i,i+needle.length)==needle)return i;
+      }
+
+return -1;
+
 };
