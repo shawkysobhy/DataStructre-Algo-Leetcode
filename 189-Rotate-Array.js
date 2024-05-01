c325b4756ee1b92732cc5eef3e027743d8831969
Shawky Sobhy <94941783+shawkysobhy@users.noreply.github.com>
2024-05-01 14:46:13 +0300

    [189] [Time Beats: 52.89%] [Memory Beats: 56.02%] - LeetPush

--- 189-Rotate-Array.js
+++ 189-Rotate-Array.js
@@ -3,22 +3,18 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+const reverse=(nums,l,r)=>{
+while(l<r){
+    [nums[r],nums[l]]=[nums[l],nums[r]]
+    l++,r--
+    }
+}
 var rotate = function(nums, k) {
     k=k%nums.length
     let l=0,r=nums.length-1
-    while(l<r){
-    [nums[r],nums[l]]=[nums[l],nums[r]]
-    l++,r--
-    }
+    reverse( nums,l,r)
     l=0,r=k-1
-     while(l<r){
-     [nums[r],nums[l]]=[nums[l],nums[r]]
-    l++,r--
-     }
-
+    reverse(nums,l,r)
     l=k,r=nums.length-1
-     while(l<r){
-    [nums[r],nums[l]]=[nums[l],nums[r]]
-    l++,r--
-    }
+    reverse(nums,l,r)
 };
